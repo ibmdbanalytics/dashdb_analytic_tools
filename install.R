6bb0e4ff72cdb7f4fbaa4c0a69ac643e2c14e3ac
torsten@de.ibm.com <torsten@de.ibm.com>
2015-12-11 14:45:58 +0100

    Added ibmdbRXt

--- install.R
+++ install.R
@@ -1,3 +1,4 @@
 install.packages('RODBC')
 install.packages('ibmdbR')
-
+install.packages('sp')
+install.packages('https://github.com/ibmdbanalytics/ibmdbRXt/blob/master/ibmdbRXt_1.47.1.tar.gz?raw=T',repos=NULL)
