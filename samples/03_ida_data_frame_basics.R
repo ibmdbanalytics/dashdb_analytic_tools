45854d4edcfa87127818c8c81ba9866085cf092b
mwurst <mwurst@users.noreply.github.com>
2016-06-22 19:07:14 +0200

    Update 03_ida_data_frame_basics.R

--- samples/03_ida_data_frame_basics.R
+++ samples/03_ida_data_frame_basics.R
@@ -1,6 +1,19 @@
 library(ibmdbR)
 
-con <- idaConnect('BLUDB','','')
+######################################################################
+## Set the connection information corresponding to you dashDB instance
+## You will find the connection informatation 
+## under Connect -> Connection Information in the dashDB web ui
+######################################################################
+host.name <- ""
+user.name <-""
+pwd <- ""
+######################################################################
+
+if((nchar(host.name)==0)||(nchar(user.name)==0)||(nchar(pwd)==0))
+  stop("Please specify the host.name, user.name and pwd by setting the variables above.")
+
+con <- idaConnect(paste("DASHDB",";Database=BLUDB;Hostname=",host.name,";Port=50000;PROTOCOL=TCPIP;UID=", user.name,";PWD=",pwd,sep=""),"","")
 idaInit(con)
 
 #Upload the table if it does not exist yet
