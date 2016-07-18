f5e7e0e57c8e459ac2073069a470f29b5335e2cf
okoeth <okoeth@de.ibm.com>
2016-07-18 09:37:41 +0200

    perform sbt build during export

--- dashdblocal_notebooks/src/sparkapp_bundler/jupyter_cms_sparkapp/scala_sparkapp.tpl
+++ dashdblocal_notebooks/src/sparkapp_bundler/jupyter_cms_sparkapp/scala_sparkapp.tpl
@@ -1,7 +1,19 @@
 {%- extends 'null.tpl' -%}
 
 {% block header %}
+
 // Spark scala application
+
+import org.apache.spark._
+import org.apache.spark.sql._
+
+object SampleApp {
+  def main(arguments: Array[String]) {
+  
+    val sparkConf = new SparkConf().setAppName("SampleApp")
+    val sc = new SparkContext(sparkConf)
+    val sqlContext = SQLContext.getOrCreate(sc)
+
 {% endblock header %}
 
 {% block in_prompt %}
@@ -15,3 +27,8 @@
 {% block markdowncell scoped %}
 {{ cell.source | comment_lines('//') }}
 {% endblock markdowncell %}
+
+{% block footer %}
+  }
+} 
+{% endblock footer %}
--- dashdblocal_notebooks/src/sparkapp_bundler/jupyter_cms_sparkapp/sparkapp_bundler.py
+++ dashdblocal_notebooks/src/sparkapp_bundler/jupyter_cms_sparkapp/sparkapp_bundler.py
@@ -1,4 +1,5 @@
 import os    
+from subprocess import call
 from nbconvert import TemplateExporter
 from jinja2 import FileSystemLoader
 
@@ -22,16 +23,25 @@ def bundle(handler, absolute_notebook_path):
     #TEMPORARY for development
     absolute_notebook_path = '/home/jovyan/work/notebooks/Spark_KMeansSample.ipynb'
     
-    notebook_filename = os.path.basename(absolute_notebook_path)
+    notebook_filename = os.path.splitext(os.path.basename(absolute_notebook_path))[0]
+    appname = notebook_filename
     
     exporter = TemplateExporter(extra_loaders=[FileSystemLoader(INSTALLDIR)])
     exporter.template_file = 'scala_sparkapp'
     
     (body, resources) = exporter.from_file(absolute_notebook_path)
 
-    with open(SOURCEFILE, 'w') as sourcefile:
+    with open(SOURCEFILE, 'wt') as sourcefile:
         sourcefile.write(body)
 
     print("noteboook exported to {0}".format(SOURCEFILE))
+    
+    print("building scala application in {0}".format(APPDIR))
+    with open(APPDIR+"/../build.sbt", "rt") as buildfile_in:
+        with open(APPDIR+"build.sbt", "wt") as buildfile_out:
+            for line in buildfile_in:
+                buildfile_out.write(line.replace('<appname>', appname))
+
+    call(["sbt", "-mem", "256", "clean", "package"], cwd=APPDIR)
         
     handler.finish("exported!")
