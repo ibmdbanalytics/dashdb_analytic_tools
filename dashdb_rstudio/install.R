005a8e5f9846016140ae9065d6500c6a64281171
Oliver Köth <okoeth@de.ibm.com>
2016-11-09 11:57:52 +0100

    install spark and sparklyr in R container

--- dashdb_rstudio/install.R
+++ dashdb_rstudio/install.R
@@ -2,3 +2,5 @@ install.packages('RODBC')
 install.packages('ibmdbR')
 install.packages('sp')
 install.packages('https://github.com/ibmdbanalytics/ibmdbRXt/blob/master/ibmdbRXt_1.47.1.tar.gz?raw=T',repos=NULL)
+install.packages('sparklyr')
+
