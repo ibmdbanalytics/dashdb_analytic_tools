eb5d3ed408c32eb0421f6eb9f1ebf9309baea365
Oliver Köth <okoeth@de.ibm.com>
2017-02-06 13:34:07 +0100

    install IPython in dashDB if required

--- dashdblocal_notebooks/launch-with-idax.sh
+++ dashdblocal_notebooks/launch-with-idax.sh
@@ -1,15 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # (c) Copyright IBM Corporation 2016
 # LICENSE: BSD-3, https://opensource.org/licenses/BSD-3-Clause
 
+set -e
+
 # verifies that required environment variables are defined and we can connect
 # to dashDB analytics REST API on target host
-verify-sparkenv.py || exit 1
+verify-sparkenv.py
 
 # upload toree server and ipython scripts to dashDB spark environment
-upload-sparkapp.py /usr/local/lib/toree.jar || exit 1
-upload-sparkapp.py /usr/local/bin/ipython-launcher.py || exit 1
-upload-sparkapp.py /usr/local/bin/startup-ipython-notebook.py || exit 1
+upload-sparkapp.py $HOME/resources/toree.jar
+upload-sparkapp.py $HOME/resources/ipython-launcher.py
+upload-sparkapp.py $HOME/resources/ipython-installer.py
+upload-sparkapp.py $HOME/resources/startup-ipython-notebook.py
+
+# verifies that IPython can be used in the dashDB spark environment
+verify-ipython-in-dashdb.py 
 
 # patch Jupyter UI to display dashDB user
 patch-ui.py
---
+++ dashdblocal_notebooks/verify-ipython-in-dashdb.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# (c) Copyright IBM Corporation 2016
+# LICENSE: BSD-3, https://opensource.org/licenses/BSD-3-Clause
+
+# Use the dashDB analytics REST API to check that the dashDB Spark support
+# is configured and accessible
+
+import warnings
+import sys, os
+import json, requests
+from requests.auth import HTTPBasicAuth
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+def run_installer():
+	auth = HTTPBasicAuth(DASHDBUSER, DASHDBPASS)
+	request_data = { 'appResource' : 'ipython-installer.py' }
+	resp = session.post("https://{0}:8443/dashdb-api/analytics/public/apps/submit".format(DASHDBHOST),
+		json=request_data, auth=auth, verify=False)
+
+	if (resp.status_code == requests.codes.unauthorized):
+		sys.exit("Could not authenticate to {0} as user {1}. Verify DASHDBUSER and DASHDBPASS information"
+				.format(DASHDBHOST, DASHDBUSER))
+	# we're exepcting an error statuscode here, because we use this to return information
+	# so there is no success statuscode check
+	try:
+		resp_data = resp.json()
+		message = resp_data['exitInfo']['message']
+		if (not message.startswith("Success:")):
+			sys.exit("IPython install failed: {0}".format(message))
+		print(message)
+		return message
+	except:
+		# json decoding error or unexpected json
+		sys.exit("Failed to submit IPython install job with status {0}: {1}"
+				.format(resp.status_code, resp.text))
+
+if __name__ == "__main__":
+	DASHDBHOST = os.environ.get('DASHDBHOST')
+	DASHDBUSER = os.environ.get('DASHDBUSER')
+	DASHDBPASS = os.environ.get('DASHDBPASS')
+	if(not DASHDBHOST): DASHDBHOST='localhost'; print("Using default localhost for DASHDBHOST")
+	if (not DASHDBUSER or not DASHDBPASS): sys.exit("DASHDBUSER and DASHDBPASS variables must be defined")
+	
+	warnings.filterwarnings('ignore', category=InsecureRequestWarning)
+	
+	try:
+		print("Verifying IPython installation on {0}".format(DASHDBHOST))
+		session = requests.Session()
+		message = run_installer();
+		if ("IPython installed" in message):
+			# had to be installed first; try again if it is available now
+			message = run_installer();
+			if (not "IPython available" in message):
+				sys.exit("IPython was installed but could not be used")
+		elif (not "IPython available" in message):
+			sys.exit("Error checking for IPython kernel")
+		
+		print("Can use IPython kernel.")
+	except requests.exceptions.ConnectionError:
+		sys.exit("Could not connect to dashDB server {0}".format(DASHDBHOST))
