e067894c9f3ab4a89ffdd7e2ad9f81dc86e63d54
Oliver Köth <okoeth@de.ibm.com>
2017-03-15 10:18:59 +0100

    fix IoT producer script

--- dashdblocal_notebooks/iot_producer/start.sh
+++ dashdblocal_notebooks/iot_producer/start.sh
@@ -19,10 +19,10 @@ zookeeper/bin/zkServer.sh start
 kafka/bin/kafka-server-start.sh -daemon kafka/config/server.properties
 
 sleep 5
-echo Create Topic
+echo "Create Topic"
 kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic iot4dashdb
 
-echo LIST of TOPICS:
+echo "LIST of TOPICS:"
 kafka/bin/kafka-topics.sh --list --zookeeper localhost:2181
 
 
@@ -32,7 +32,6 @@ TEMP_MIN=30
 TEMP_OUT=18
 TEMP_RANGE=9
 ID=2700
-COUNTER=3
 SLEEPS=0.05
 POWER_LVL=1827
 DEV_TYPE="windGen"
@@ -47,16 +46,21 @@ if [ "$#" -ge 2 ]; then
     TEMP_MIN="$5"
 fi
 
-while [  $COUNTER -ge 1 ]; do
+echo "Starting producer loop for $COUNTER messages"
 
-    COUNTER=COUNTER-1
-    DEV_ID=$ID+$RANDOM%10
+while [ $COUNTER -ge 1 ]; do
+
+    let COUNTER=COUNTER-1
+    let DEV_ID=ID+$RANDOM%10
     TS=`date +"%F %T"`
-    TEMP=$TEMP_MIN+$RANDOM%$TEMP_RANGE
-    POWER=$POWER_LVL+$RANDOM%10
+    let TEMP=TEMP_MIN+$RANDOM%TEMP_RANGE
+    let POWER=POWER_LVL+$RANDOM%10
 
     JSON="{\"payload\": {\"temperature\": $TEMP,\"tempOutside\": $TEMP_OUT,\"powerProd\": $POWER,\"noiseLevel1\": $NOISE,\"time\":\"$TS\"},\"deviceId\": \"$DEV_ID\",\"deviceType\": \"$DEV_TYPE\",\"eventType\": \"status\"}"
     echo $JSON | kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic iot4dashdb
 
     sleep $SLEEPS
 done
+
+echo "Producer finished"
+
